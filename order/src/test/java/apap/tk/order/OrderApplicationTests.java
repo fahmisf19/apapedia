3b14229b2a7b3611e0d136e4492056c937565805
fahmi.sabila <fahmi.sabila@ui.ac.id>
2023-11-08 11:23:10 +0700

    add projects

---
+++ order/src/test/java/apap/tk/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tk.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ user/src/test/java/apap/tk/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tk.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
