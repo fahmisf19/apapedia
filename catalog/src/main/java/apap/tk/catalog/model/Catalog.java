da0497a7c4a241af82f72c1a86b6acdbb427e951
Your Namegit <you@example.com>
2023-11-11 16:44:02 +0700

    feat: add fitur #5 dan #6 catalog service

---
+++ catalog/src/main/java/apap/tk/catalog/model/Catalog.java
@@ -0,0 +1,55 @@
+package apap.tk.catalog.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "catalog")
+public class Catalog {
+    @Id
+    private UUID id = UUID.randomUUID();
+    
+    // msh belom pix
+    @NotNull
+    @Column(name = "seller", nullable = false)
+    private UUID seller;
+    
+    @NotNull
+    @Column(name = "price", nullable = false)
+    private int price;
+    
+    @NotNull
+    @Column(name = "productName", nullable = false)
+    private String productName;
+    
+    @NotNull
+    @Column(name = "productDescription", nullable = false)
+    private String productDescription;
+  
+    @NotNull
+    @Column(name = "stock", nullable = false)
+    private int stock;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_category", referencedColumnName = "idCategory")
+    Category category;
+
+    @NotNull
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted = false;
+
+    @NotNull
+    @Column(name = "image", nullable = false)
+    private byte[] image;
+    
+}
+
---
+++ catalog/src/main/java/apap/tk/catalog/restcontroller/CatalogRestController.java
@@ -0,0 +1,58 @@
+package apap.tk.catalog.restcontroller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import apap.tk.catalog.dto.CatalogMapper;
+import apap.tk.catalog.dto.request.UpdateCatalogDTO;
+import apap.tk.catalog.model.Catalog;
+import apap.tk.catalog.restservice.CatalogRestService;
+
+@RestController
+@RequestMapping("/api")
+public class CatalogRestController {
+    @Autowired
+    private CatalogMapper catalogMapper;
+
+    @Autowired
+    private CatalogRestService catalogRestService;
+
+    @PutMapping(value = "/catalog/{idCatalog}")
+    public Catalog updateCatalog(@PathVariable("idCatalog") UUID idCatalog, @RequestBody UpdateCatalogDTO updateCatalogDTO, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Request body has an invalid type or missing field");
+        } else {
+            var updateCatalog = catalogMapper.updateCatalogRequestDTOToCatalog(updateCatalogDTO); // Implement the mapping method.
+            var catalog = catalogRestService.updateRestCatalog(idCatalog, updateCatalog); // Implement the update service method.
+            return catalog; // Return the updated catalog as JSON response.
+        }
+    }
+
+    @DeleteMapping(value = "/catalog/{idCatalog}")
+    public ResponseEntity<String> restDeleteCatalog(@PathVariable("idCatalog") UUID idCatalog) {
+        // Cek apakah Catalog dengan ID yang sesuai ada dalam basis data
+        var catalog = catalogRestService.getRestCatalogById(idCatalog);
+        if (catalog == null) {
+            // Jika Catalog tidak ditemukan, kembalikan respons NOT FOUND
+            return ResponseEntity.notFound().build();
+        }
+
+        // Lakukan penghapusan Catalog
+        catalogRestService.deleteCatalog(catalog);
+
+        // Kembalikan respons OK jika penghapusan berhasil
+        return ResponseEntity.ok("Catalog dengan id " + idCatalog + " berhasil dihapus");
+    }
+}
