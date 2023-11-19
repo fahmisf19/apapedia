a0110768745419a96e937e11f0d7bcc2f7bdc41d
Nadira Maysa Dyandra <nadira.maysa@ui.ac.id>
2023-11-19 23:33:15 +0700

    Resolve "Pengerjaan iterasi 2 - Nadira"

---
+++ order/src/main/java/apap/tk/order/dto/request/UpdateCartItemRequestDTO.java
@@ -0,0 +1,18 @@
+package apap.tk.order.dto.request;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UpdateCartItemRequestDTO{
+    @NotNull
+    private UUID cartItemId;
+    private Integer newQuantity;    
+}
