b04a17554692c27a78fefde64d4c78cd447b7aeb
Nadira Maysa Dyandra <nadira.maysa@gmail.com>
2023-11-27 21:26:23 +0700

    iterasi 3: add raw fitur order-history

---
+++ frontendweb/src/main/java/apap/tk/frontendweb/service/OrderService.java
@@ -0,0 +1,11 @@
+package apap.tk.frontendweb.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+import apap.tk.frontendweb.dto.response.order.ReadOrderResponseDTO;
+
+public interface OrderService {
+    List<ReadOrderResponseDTO> getListOrder(UUID sellerId) throws IOException, InterruptedException;
+}
