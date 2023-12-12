206aec5b947ac120fdee808969c0c6df36c49b17
Nadira Maysa Dyandra <nadira.maysa@gmail.com>
2023-12-12 16:33:23 +0700

    Iterasi 3: update user

--- user/src/main/java/apap/tk/user/restservice/UserRestService.java
+++ user/src/main/java/apap/tk/user/restservice/UserRestService.java
@@ -1,9 +1,6 @@
 package apap.tk.user.restservice;
 
-<<<<<<< HEAD
 import apap.tk.user.dto.request.UpdateUserRequestDto;
-=======
->>>>>>> d31e0feb0e76c049ff3e109873f2753fdc21bff3
 import apap.tk.user.model.UserEntity;
 import java.util.List;
 import java.util.Optional;
@@ -13,9 +10,6 @@ public interface UserRestService {
     void createRestUser(UserEntity user);
     List<UserEntity> retrieveRestAllUser();
     UserEntity getRestUserById(UUID id);
-<<<<<<< HEAD
     // UserEntity updateUser(UpdateUserRequestDto updatedUser, String token);
     void deleteUser(UUID id, String token);
-=======
->>>>>>> d31e0feb0e76c049ff3e109873f2753fdc21bff3
 }
